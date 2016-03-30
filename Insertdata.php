6928f8068bc47e7548cfa2afd7be2d7b19ec07c4
Munib-Deshmukh <muneebdeshmukh@gmail.com>
2016-03-30 11:31:59 +0500

    Update Insertdata.php

--- Insertdata.php
+++ Insertdata.php
@@ -6,13 +6,13 @@ include 'Core/init.php';
 //	print_r($_POST);
 
 
-    mysql_query("INSERT INTO `mis-project`.`invoice` (`order_id`, `invoice_name`,`Total_amount`) VALUES (NULL, '{$_POST['re_name']}',{$_POST['Total']})");
+    mysql_query("INSERT INTO `sql6112808`.`invoice` (`order_id`, `invoice_name`,`Total_amount`) VALUES (NULL, '{$_POST['re_name']}',{$_POST['Total']})");
     $id=mysql_insert_id();
 
     for($i=0;$i<count($_POST['ProductName']);$i++){
         echo count($_POST['ProductName']);
 //        die();
-        mysql_query("INSERT INTO `mis-project`.`invoice_detail` (`ID`, `order_id`, `productname`, `quantity`, `price`, `discount`, `amount`)
+        mysql_query("INSERT INTO `sql6112808`.`invoice_detail` (`ID`, `order_id`, `productname`, `quantity`, `price`, `discount`, `amount`)
                         VALUES (NULL, '{$id}', '{$_POST['ProductName'][$i]}', '{$_POST['Quantity'][$i]}'
                         , '{$_POST['Price'][$i]}', '{$_POST['Discount'][$i]}', '{$_POST['Amount'][$i]}')");
     }
